d15518334014464dd9c80365e2bd092a9fd3d5e5
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2021-05-02 13:21:58 -0700

    update

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -1,26 +1,83 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require("express");
+const db = require("../data/config");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  try {
+    const messages = await db.select("*").from("messages");
+    res.status(200).json(messages);
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+router.get("/:id", async (req, res, next) => {
+  try {
+    const [message] = await db
+      .select("*")
+      .from("messages")
+      .where("id", req.params.id)
+      .limit(1);
+    res.status(200).json(message);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get("/:id", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+  try {
+    const payload = {
+      title: req.body.title,
+      contents: req.body.contents,
+    };
 
-})
+    if (!payload.title || !payload.contents) {
+      return res.status(400).json({ message: "Need title and contents" });
+    }
 
-router.post("/", (req, res, next) => {
+    const [id] = await db.insert(payload).into("messages");
+    const message = await db
+      .select("*")
+      .from("messages")
+      .where("id", id)
+      .limit(1);
+    res.status(201).json(message);
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+router.put("/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      title: req.body.title,
+      contents: req.body.contents,
+    };
 
-router.put("/:id", (req, res, next) => {
+    if (!payload.title || !payload.contents) {
+      return res.status(400).json({ message: "Need title and contents" });
+    }
 
-})
+    await db("messages").where("id", req.params.id).update(payload);
 
-router.delete("/:id", (req, res, next) => {
+    const message = await db
+      .first("*")
+      .from("messages")
+      .where("id", req.params.id);
 
-})
+    res.json(message);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+router.delete("/:id", async (req, res, next) => {
+  try{
+
+  }catch(err){next(err)}
+  await db("messages").where("id",req.params.id).del()
+  res.status(204).end()
+});
+
+module.exports = router;
